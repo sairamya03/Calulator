cf4e208809454a250539267919184910bea049ff
Machavarapu <smachav@apac.corpdir.net>
2019-12-17 14:35:41 +0530

    Multiplication Operation

---
+++ Multiplication.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 15:49:33 2019
+
+@author: SMACHAV
+"""
+
+def multiply(x, y):
+   return x * y
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(num1,"*",num2,"=", multiply(num1,num2))
