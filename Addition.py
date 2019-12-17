0f627f5c5bc02150ef1a1c14b5a7c11cf56726dd
Machavarapu <smachav@apac.corpdir.net>
2019-12-17 14:30:07 +0530

    Addition Operation

---
+++ Addition.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 15:48:23 2019
+
+@author: SMACHAV
+"""
+
+def add(x, y):
+   return x + y
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(num1,"+",num2,"=", add(num1,num2))
