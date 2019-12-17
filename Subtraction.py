a222c985fc25e6e04d1b765e6685f6da04a09083
Machavarapu <smachav@apac.corpdir.net>
2019-12-17 14:31:22 +0530

    Subtraction Operation

---
+++ Subtraction.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 15:48:27 2019
+
+@author: SMACHAV
+"""
+
+def subtract(x, y):
+   return x - y
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+    
+print(num1,"-",num2,"=", subtract(num1,num2))
